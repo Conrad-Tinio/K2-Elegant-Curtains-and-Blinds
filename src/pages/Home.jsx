a0737cd63dac365cc372b6f40c64d767a97370f0
Conrad-Tinio <lstinio@up.edu.ph>
2025-06-30 22:17:05 +0800

    configured Home.jsx for each component; edited css styling for better UI/UX

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import ProjectsSection from '../components/ProjectsSection'
+import ContactSection from '../components/ContactSection'
 
 export default function Home() {
   return (
     <div className='min-h-screen'>
         {/* Navbar */}
-            <Navbar />
+        <Navbar />
+
+        {/* Main Content */}
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <ProjectsSection />
+            <ContactSection />
+        </main>
+
+        {/* Footer */}
+        <Footer /> 
     </div>
   )
 }

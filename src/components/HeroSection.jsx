fdcd961bccd86eb66c02e0e912b75fbbca78c71d
Conrad-Tinio <lstinio@up.edu.ph>
2025-06-30 23:11:35 +0800

    fixed file paths issues on images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <section id="hero" className='relative min-h-screen flex flex-col items-center justify-center px-4'>
-        <img src='/logo.svg' className='h-40 w-40 items-center justify-center flex mt-5'/>
+        <img src='./logo.svg' className='h-40 w-40 items-center justify-center flex mt-5'/>
         <div className='container max-w-4xl mx-auto text-center z-10'>
             <div className='space-y-8'>
                 <h1 className='font-semibold text-4xl md:text-6xl tracking-tight'>

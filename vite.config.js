c60cbbf6f8315c4d4616c9c4be3f1ce84db2ec2d
Conrad-Tinio <lstinio@up.edu.ph>
2025-06-30 22:50:41 +0800

    fixed mime issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 })
